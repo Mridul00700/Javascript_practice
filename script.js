c593761779d403fd0f5b881810878339afcd25ce
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-06-28 13:46:44 +0530

    Class-eg

--- script.js
+++ script.js
@@ -520,22 +520,91 @@ DATA CAR 2: 'Mercedes' going at 95 km/h
 
 
 
-// Object.create ---->>> 
-// person prototype ------
-const PersonProto = {
-    calcAge() {
-        console.log(2021 - this.birthYear);
-    }
-};
+// // Object.create ---->>> 
+// // person prototype ------
+// const PersonProto = {
+//     calcAge() {
+//         console.log(2021 - this.birthYear);
+//     },
+//     init(name, birthYear) {
+//         this.name = name;
+//         this.birthYear = birthYear;
+//     }
+// };
 
-// Creating the object person with prototype of Personproto
-// prototypal inheritance ----->>>>     least used way to create prototypal inheritances
-const rake = Object.create(PersonProto);
-console.log(rake);
+// // Creating the object person with prototype of Personproto
+// // prototypal inheritance ----->>>>     least used way to create prototypal inheritances
+// const rake = Object.create(PersonProto);
+// console.log(rake);
 
-rake.name = 'Rake'
-rake.birthYear = 2010;
-rake.calcAge();
+// // rake.name = 'Rake'
+// // rake.birthYear = 2010;
+// rake.init('Rake', 2010);
+// rake.calcAge();
 
-console.log(rake.__proto__);
+// console.log(rake.__proto__);
+
+
+// const sara = Object.create(PersonProto);
+
+// sara.init('Sara', 2005);
+// sara.calcAge();
+
+/*
+1. Use a ES6 Class to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+*/
+
+// class Car {
+//     constructor(make, speed) {
+//         this.make = make;
+//         this.speed = speed;
+//     }
+
+//     accelerate() {
+//         this.speed += 10;
+//         console.log("km/hr", this.speed);
+//     }
+
+//     brake() {
+//         this.speed -= 5;
+//         console.log("km/hr", this.speed);
+//     }
+
+//     get speedUS() {
+//         // console.log(this._speed);
+//         return (this.speed / 1.6)
+
+//     }
+//     set speedUS(speed) {
+//         this.speed = speed * 1.6;
+//     }
+// }
+
+// const Ford = new Car('Ford', 120);
+// console.log(Ford.speedUS);
+// Ford.accelerate();
+// Ford.accelerate();
+// Ford.accelerate();
+// Ford.accelerate();
+// console.log(Ford.speedUS);
+
+// Ford.accelerate();
+// console.log(Ford.speedUS);
+
+// Ford.brake();
+// Ford.brake();
+// Ford.brake();
+// console.log(Ford.speedUS);
+// Ford.brake();
+// Ford.brake();
+// console.log(Ford.speedUS);
+// Ford.speedUS = 150
+// console.log(Ford);
 
