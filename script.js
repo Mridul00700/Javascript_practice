261aae85bc4f8b3d01b1d53da0feebbb9a57c16c
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-07-20 11:15:59 +0530

    Async-await-countries

--- script.js
+++ script.js
@@ -1069,14 +1069,39 @@ TEST COORDINATES 2: -33.933, 18.474
 // }).then(() => {
 //     image.style.display = 'none'
 // }).catch(err => console.error(err));
+const countriesContainer = document.querySelector('.countries');
+
+
+const renderCountry = (data, className = '') => {
+    const html =
+        `<article class="country ${className}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}M people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+          </div>
+        </article>`
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    // countriesContainer.style.opacity = 1;
+}
+
 
 // Async - Await 
 
 // Async is running in background not in call stack
 const whereAmI = async function (country) {
 
+    // fetch(`https://restcountries.eu/rest/v2/name/${country}?fullText=true`).then(res => console.log(res));
+
     const res = await fetch(`https://restcountries.eu/rest/v2/name/${country}?fullText=true`);
-    console.log(res);
+    const data = await res.json();
+    console.log(data);
+    renderCountry(data[0]);
+
 }
 
 whereAmI('portugal');
