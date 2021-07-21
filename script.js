885dd05993fa59b7013e87d500277fb23197417c
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-07-21 09:19:53 +0530

    Async-example

--- script.js
+++ script.js
@@ -1145,16 +1145,16 @@ TEST COORDINATES 2: -33.933, 18.474
 
 // const renderError = function (msg) {
 //     countriesContainer.insertAdjacentText('beforeend', msg)
+// // };
+// // // converts response to Json data.
+// const getJson = function (url, errormessage = "Something went wrong") {
+//     return fetch(url).then(response => {
+//         if (!response.ok) {
+//             throw new Error(`${errormessage} ${response.status}`)
+//         }
+//         return response.json()
+//     });
 // };
-// // converts response to Json data.
-const getJson = function (url, errormessage = "Something went wrong") {
-    return fetch(url).then(response => {
-        if (!response.ok) {
-            throw new Error(`${errormessage} ${response.status}`)
-        }
-        return response.json()
-    });
-};
 
 // const get3Countries = async function (c1, c2, c3) {
 
@@ -1177,44 +1177,132 @@ const getJson = function (url, errormessage = "Something went wrong") {
 //     } catch (err) {
 //         console.log(err);
 //     }
-// }
+// // }
 
-// get3Countries('portugal', 'canada', 'india');
+// // get3Countries('portugal', 'canada', 'india');
 
-// Promise.race  First settled promise wins the race 
-// Very long running promise
-(async function () {
-    const response = await Promise.race([
-        getJson(`https://restcountries.eu/rest/v2/name/india?fullText=true`),
-        getJson(`https://restcountries.eu/rest/v2/name/usa?fullText=true`),
-        getJson(`https://restcountries.eu/rest/v2/name/portugal?fullText=true`),
+// // Promise.race  First settled promise wins the race 
+// // Very long running promise
+// (async function () {
+//     const response = await Promise.race([
+//         getJson(`https://restcountries.eu/rest/v2/name/india?fullText=true`),
+//         getJson(`https://restcountries.eu/rest/v2/name/usa?fullText=true`),
+//         getJson(`https://restcountries.eu/rest/v2/name/portugal?fullText=true`),
 
-    ]);
+//     ]);
 
-    console.log(response[0]);
-})();
-// Timeout function
+//     console.log(response[0]);
+// })();
+// // Timeout function
 
-const timeout = function (sec) {
-    return new Promise(function (_, reject) {
-        setTimeout(function () {
-            reject(new Error('Request took too long'));
-        }, sec * 1000)
+// const timeout = function (sec) {
+//     return new Promise(function (_, reject) {
+//         setTimeout(function () {
+//             reject(new Error('Request took too long'));
+//         }, sec * 1000)
+//     });
+// };
+
+// Promise.race([
+//     getJson(`https://restcountries.eu/rest/v2/name/india?fullText=true`),
+//     timeout(0.1)
+// ]).then(res => console.log(res[0])).catch(err => console.log(err))
+
+
+// // Promise.allSettled   return the array of all the settled promises wheather it fails or is a success.. unlike all as it just shorts circuits when one is rejected. 
+
+// Promise.allSettled([
+//     Promise.resolve('Success'),
+//     Promise.reject('Error'),
+//     Promise.resolve('Success')
+// ]).then(res => console.log(res));
+
+// // Promise.any [ES2021]
+
+// Promise.any([
+//     Promise.resolve('Success'),
+//     Promise.reject('Error'),
+//     Promise.resolve('Success')
+// ]).then(res => console.log(res));
+
+
+const wait = function (seconds) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, seconds * 1000);
     });
 };
 
-Promise.race([
-    getJson(`https://restcountries.eu/rest/v2/name/india?fullText=true`),
-    timeout(0.1)
-]).then(res => console.log(res[0])).catch(err => console.log(err))
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+    return new Promise(function (resolve, reject) {
+        const img = document.createElement('img');
+        img.src = imgPath;
+
+        img.addEventListener('load', function () {
+            imgContainer.append(img);
+            resolve(img);
+        });
+
+        img.addEventListener('error', function () {
+            reject(new Error('Image not found'));
+        });
+    });
+};
+
+// let currentImg;
+
+// createImage('img/img-1.jpg')
+//     .then(img => {
+//         currentImg = img;
+//         console.log('Image 1 loaded');
+//         return wait(2);
+//     })
+//     .then(() => {
+//         currentImg.style.display = 'none';
+//         return createImage('img/img-2.jpg');
+//     })
+//     .then(img => {
+//         currentImg = img;
+//         console.log('Image 2 loaded');
+//         return wait(2);
+//     })
+//     .then(() => {
+//         currentImg.style.display = 'none';
+//     })
+//     .catch(err => console.error(err));
 
 
-// Promise.allSettled   return the array of all the settled promises wheather it fails or is a success.. unlike all as it just shorts circuits when one is rejected. 
+// PART 1
+// Write an async function 'loadNPause' that recreates Coding Challenge #2, this time using async / await(only the part where the promise is consumed).Compare the two versions, think about the big differences, and see which one you like more.
+//     Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Network tab.
 
-Promise.allSettled([
-    Promise.resolve('Success'),
-    Promise.reject('Error'),
-    Promise.resolve('Success')
-]).then(res => console.log(res))
+// PART 2
+// 1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+// 2. Use.map to loop over the array, to load all the images with the 'createImage' function (call the resulting array 'imgs')
+// 3. Check out the 'imgs' array in the console! Is it like you expected ?
+//     4. Use a promise combinator function to actually get the images from the array 😉
+// 5. Add the 'paralell' class to all the images(it has some CSS styles).
 
+// TEST DATA: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg'].To test, turn off the 'loadNPause' function.
+
+// let currentImg;
+// const loadnPause = async function () {
+//     try {
+//         currentImg = await createImage('./img-1.jpg');
+//         console.log('Image 1 loaded');
+//         await wait(2);
+//         currentImg.style.display = 'none';
+//         currentImg = await createImage('./img-2.jpg');
+//         console.log("image 2 loaded");
+//         await wait(2);
+//         currentImg.style.display = 'none';
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }
+// }
+
+
+// loadnPause();
 
