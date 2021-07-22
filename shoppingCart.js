bbc0917a4ec1c8ec25b677e6e845a1b3ea488a4b
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-07-22 09:22:36 +0530

    CommonJS

--- shoppingCart.js
+++ shoppingCart.js
@@ -1,23 +1,31 @@
-// Exporting Module
-console.log("Exporting Module");
+// // Exporting Module
+// console.log("Exporting Module");
 
-const shippingCost = 10;
-export const cart = [];
+// const shippingCost = 10;
+// export const cart = [];
 
-// Named and default exports 
-// Named
-export const addToCart = (product, quantity) => {
-    cart.push({ product, quantity });
-    console.log(`${quantity} ${product} was added to the cart.`);
-}
+// // Named and default exports 
+// // Named
+// export const addToCart = (product, quantity) => {
+//     cart.push({ product, quantity });
+//     console.log(`${quantity} ${product} was added to the cart.`);
+// }
 
-const totalPrice = 237;
-const totalQuantity = 23;
+// const totalPrice = 237;
+// const totalQuantity = 23;
 
-export { totalPrice, totalQuantity as tq };
+// export { totalPrice, totalQuantity as tq };
 
-// No name is involved in exporting default
-export default function (product, quantity) {
+// // No name is involved in exporting default
+// export default function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(`${quantity} ${product} was added to the cart.`);
+// }
+
+
+// CommonJS
+
+export.addToCart = function (product, quantity) {
     cart.push({ product, quantity });
     console.log(`${quantity} ${product} was added to the cart.`);
 }
